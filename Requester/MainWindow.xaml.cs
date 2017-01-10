bbc796427c2396b4b11c17a45a674efbb0e7d549
mazik7 <mazik7@outlook.com>
2017-01-10 18:59:39 +0700

    Допилил гет запросы

--- Requester/MainWindow.xaml.cs
+++ Requester/MainWindow.xaml.cs
@@ -47,7 +47,21 @@ namespace Requester
             _tests.Add(new Test(_testsClient, new RestRequest("/organizations/", Method.GET), "4.1 Список организаций", "Проверка на отработку запроса \"/organizations/\""));
             _tests.Add(new Test(_testsClient, new RestRequest("/workspaces/?organizationKey=techrunning", Method.GET), "4.2 Список рабочих областей в организации", "Проверка на отработку запроса \"/workspaces/?organizationKey=orgKey/\""));
             _tests.Add(new Test(_testsClient, new RestRequest("/comments/?targetType=ApplicationPartRecord&targetKey=51761", Method.GET), "4.3.1 Комментарии элемента приложения", "Проверка на отработку запроса \"/comments/?targetType=type&targetKey=key\" \n для типа \"ApplicationPartRecord\"(Элемент приложения)"));
+            _tests.Add(new Test(_testsClient, new RestRequest("/comments/?targetType=StatusData&targetKey=251938924079240_1727685f-37c4-412e-9baa-25e489bf88d0", Method.GET), "4.3.2 Комментарии статуса", "Проверка на отработку запроса \"/comments/?targetType=type&targetKey=key\" \n для типа \"StatusData\"(Статус)"));
+            _tests.Add(new Test(_testsClient, new RestRequest("/comments/?targetType=Organization&targetKey=techrunning", Method.GET), "4.3.3 Комментарии контекста организации", "Проверка на отработку запроса \"/comments/?targetType=type&targetKey=key\" \n для типа \"Organization\"(Организация)"));
+            _tests.Add(new Test(_testsClient, new RestRequest("/comments/?targetType=UserOrganization&targetKey=23658|techrunning", Method.GET), "4.3.4 Комментарии контекста пользователя в организации", "Проверка на отработку запроса \"/comments/?targetType=type&targetKey=key\" \n для типа \"UserOrganization\"(Пользователь в организации)"));
+            _tests.Add(new Test(_testsClient, new RestRequest("/comments/?targetType=Workspace&targetKey=techrunning|workspace", Method.GET), "4.3.5 Комментарии контекста рабочей области", "Проверка на отработку запроса \"/comments/?targetType=type&targetKey=key\" \n для типа \"Workspace\"(Рабочая область)"));
+            _tests.Add(new Test(_testsClient, new RestRequest("/comments/?targetType=UserWorkspace&targetKey=23658|techrunning||workspace", Method.GET), "4.3.6 Комментарии контекста пользователя в рабочей области", "Проверка на отработку запроса \"/comments/?targetType=type&targetKey=key\" \n для типа \"UserWorkspace\"(Пользователь в рабочей области)"));
+            _tests.Add(new Test(_testsClient, new RestRequest("/comments/?targetType=ApplicationMetadata&targetKey=trainings", Method.GET), "4.3.7 Комментарии приложения", "Проверка на отработку запроса \"/comments/?targetType=type&targetKey=key\" \n для типа \"ApplicationMetadata\"(Приложение)"));
+            _tests.Add(new Test(_testsClient, new RestRequest("/comments/?targetType=TaskPartRecord&targetKey=54499", Method.GET), "4.3.8 Комментарии задачи", "Проверка на отработку запроса \"/comments/?targetType=type&targetKey=key\" \n для типа \"TaskPartRecord\"(Задача)"));
             _tests.Add(new Test(_testsClient, new RestRequest("/activities/?contextType=Organization&contextKey=techrunning", Method.GET), "4.5.1 Список активностей организации", "Проверка на отработку запроса \"/activities/?contextType=type&contextKey=key\" \n для типа \"Organization\"(Организация)"));
+            _tests.Add(new Test(_testsClient, new RestRequest("/activities/?contextType=ApplicationPartRecord&contextKey=51761", Method.GET), "4.5.2 Список активностей элемента приложения", "Проверка на отработку запроса \"/activities/?contextType=type&contextKey=key\" \n для типа \"ApplicationPartRecord\"(Элемент приложения)"));
+            _tests.Add(new Test(_testsClient, new RestRequest("/activities/?contextType=StatusData&contextKey=251938924079240_1727685f-37c4-412e-9baa-25e489bf88d0", Method.GET), "4.5.3 Список активностей статуса", "Проверка на отработку запроса \"/activities/?contextType=type&contextKey=key\" \n для типа \"StatusData\"(Статус)"));
+            _tests.Add(new Test(_testsClient, new RestRequest("/activities/?contextType=UserOrganization&contextKey=23658|techrunning", Method.GET), "4.5.4 Список активностей контекста пользователя в организации", "Проверка на отработку запроса \"/activities/?contextType=type&contextKey=key\" \n для типа \"UserOrganization\"(Пользователь в организации)"));
+            _tests.Add(new Test(_testsClient, new RestRequest("/activities/?contextType=Workspace&contextKey=techrunning|workspace", Method.GET), "4.5.5 Список активностей контекста рабочей области", "Проверка на отработку запроса \"/activities/?contextType=type&contextKey=key\" \n для типа \"Workspace\"(Рабочая область)"));
+            _tests.Add(new Test(_testsClient, new RestRequest("/activities/?contextType=UserWorkspace&contextKey=23658|techrunning||workspace", Method.GET), "4.5.6 Список активностей контекста пользователя в рабочей области", "Проверка на отработку запроса \"/activities/?contextType=type&contextKey=key\" \n для типа \"UserWorkspace\"(Пользователь в рабочей области)"));
+            _tests.Add(new Test(_testsClient, new RestRequest("/activities/?contextType=ApplicationMetadata&contextKey=trainings", Method.GET), "4.5.7 Список активностей приложения", "Проверка на отработку запроса \"/activities/?contextType=type&contextKey=key\" \n для типа \"ApplicationMetadata\"(Приложение)"));
+            _tests.Add(new Test(_testsClient, new RestRequest("/activities/?contextType=TaskPartRecord&contextKey=54499", Method.GET), "4.5.8 Список активностей задачи", "Проверка на отработку запроса \"/activities/?contextType=type&contextKey=key\" \n для типа \"TaskPartRecord\"(Задача)"));
             _tests.Add(new Test(_testsClient, new RestRequest("/activities/251925360948376|ef4ebebe-1227-4bc3-95f9-992784934dc0/", Method.GET), "4.6 Контекст полученный по ключу", "Проверка на отработку запроса \"/activities/contextKey/\""));
             _tests.Add(new Test(_testsClient, new RestRequest("/activities/userStream/", Method.GET), "5.1 Лента активности текущего пользователя", "Проверка на отработку запроса \"/activities/userStream/\""));
             _tests.Add(new Test(_testsClient, new RestRequest("/users/2/", Method.GET), "5.2 Получение информации о пользователе", "Подогреть админчику)0)))))/n Проверка запроса /users/userKey/"));
@@ -61,6 +75,7 @@ namespace Requester
             foreach (Test test in _tests)
             {
                 item = new ListBoxItem();
+                
                 item.Content = test.Name;
                 item.ToolTip = test.Description;
                 listBoxTests.Items.Add(item);
@@ -362,7 +377,7 @@ namespace Requester
                         }
                     }
                 }
-                textBoxLog.Text += "Запускаем несколько тестов. Придется долго ждать пока я не закончу. Вахахах" + "\n";
+                textBoxLog.AppendText("Запускаем несколько тестов. Придется долго ждать пока я не закончу. Вахахах" + "\n");
                 TestSuite suite = new TestSuite(testsList);
                 testsResult = suite.Start(_config.Token);
                 textBoxLog.Text += "Информация о прошедших тестах..." + "\n";
