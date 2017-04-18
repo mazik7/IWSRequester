bd2344112686c049e8601a95331f1214d4118039
mazik7 <mazik7@outlook.com>
2017-04-18 14:13:10 +0700

    One more change

--- Requester/Test.cs
+++ Requester/Test.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 using RestSharp;
 
 namespace Requester
@@ -21,6 +22,7 @@ namespace Requester
         private string _description;
         private string _name;
         private RestClient _client;
+        private HttpStatusCode _expectedCode;
         public State Status { get; set; }
         public RestRequest Request
         {
@@ -31,6 +33,17 @@ namespace Requester
             set
             {
                 _request = value;
+            }
+        }
+        public HttpStatusCode ExpectedCode
+        {
+            get
+            {
+                return _expectedCode;
+            }
+            set
+            {
+                _expectedCode = value;
             }
         }
         public string Name
@@ -61,6 +74,7 @@ namespace Requester
             _name = name;
             _description = description;
             _client = client;
+            _expectedCode = HttpStatusCode.OK;
             Status = State.Ready;
         }
         public Test(RestClient client, RestRequest request, string name, string description, string body)
@@ -70,6 +84,17 @@ namespace Requester
             _description = description;
             _client = client;
             _request.AddParameter("application/json", body, ParameterType.RequestBody);
+            _expectedCode = HttpStatusCode.OK;
+            Status = State.Ready;
+        }
+        public Test(RestClient client, RestRequest request, string name, string description, string body, System.Net.HttpStatusCode expectedCode)
+        {
+            _request = request;
+            _name = name;
+            _description = description;
+            _client = client;
+            _request.AddParameter("application/json", body, ParameterType.RequestBody);
+            _expectedCode = expectedCode;
             Status = State.Ready;
         }
         public Result Start(string oauth_token)
@@ -89,34 +114,12 @@ namespace Requester
                 _request.AddHeader("oauth_token", oauth_token);
                 IRestResponse responce;
                 responce = _client.Execute(_request);
-                if(responce.StatusCode==System.Net.HttpStatusCode.OK)
+                if(responce.StatusCode==_expectedCode)
                     return new Result(responce.Request.Resource ,true, responce.StatusCode.ToString(), responce.Content.ToString(), responce.StatusDescription.ToString());
                 else
                     return new Result(responce.Request.Resource, false, responce.StatusCode.ToString(), responce.Content.ToString(), responce.StatusDescription.ToString());
             }
         }
-        public Result Start(string oauth_token, System.Net.HttpStatusCode expectedCode)
-        {
-            if (Status == State.Pidor)
-                return new Result(null, false, null, null, null);
-            else
-            {
-                foreach (Parameter parameter in _request.Parameters)
-                {
-                    if (parameter.Name.Contains("oauth_token"))
-                    {
-                        _request.Parameters.Remove(parameter);
-                        break;
-                    }
-                }
-                _request.AddHeader("oauth_token", oauth_token);
-                IRestResponse responce;
-                responce = _client.Execute(_request);
-                if (responce.StatusCode == expectedCode)
-                    return new Result(responce.Request.Resource, true, responce.StatusCode.ToString(), responce.Content.ToString(), responce.StatusDescription.ToString());
-                else
-                    return new Result(responce.Request.Resource, false, responce.StatusCode.ToString(), responce.Content.ToString(), responce.StatusDescription.ToString());
-            }
-        }
+
     }
 }
